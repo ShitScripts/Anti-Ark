d5d9a95a44363a2176b4c0c37fc9195588fd2ab6
austin powers <35671313+ShitScripts@users.noreply.github.com>
2022-07-22 09:10:39 -0700

    noti

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,14 @@
-console.log("Anti Ark website made by Austin287856");
+const gulp = require("gulp");
+const notify = require("gulp-notify");
+const sweetalert = require("sweetalert2");
 
+sweetalert.fire({
+  icon: 'success',
+  title: 'ip copied to clipboard',
+  showConfirmButton: false,
+  timer: 1500
+})
+console.log("Anti Ark website made by Austin287856");
 const element = document.getElementById('ip_button');
 element.addEventListener("click", () => {
 	navigator.clipboard.writeText("play.pretty-hot.xyz").then(function() {
