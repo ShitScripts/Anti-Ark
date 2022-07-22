52a26001938ec66f2dbd42c88890b511810add36
austin powers <35671313+ShitScripts@users.noreply.github.com>
2022-07-22 04:45:12 -0700

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,8 @@
-console.log("Website by Austin287856 V1");
+console.log("Anti Ark");
 
 var btn1 = document.querySelector('.btn1');
 
 btn1.addEventListener('click', function(event) {
-  console.log('detected click')
+  console.log('detected click');
   navigator.clipboard.writeText('play.pretty-hot.xyz');
 });
