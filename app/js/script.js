2527cb7e6691c825e9c73647d90d2de758aad924
austin powers <35671313+ShitScripts@users.noreply.github.com>
2022-07-22 09:11:04 -0700

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -2,16 +2,17 @@ const gulp = require("gulp");
 const notify = require("gulp-notify");
 const sweetalert = require("sweetalert2");
 
+console.log("Anti Ark website made by Austin287856");
+const element = document.getElementById('ip_button');
+element.addEventListener("click", () => {
+	navigator.clipboard.writeText("play.pretty-hot.xyz").then(function() {
+
 sweetalert.fire({
   icon: 'success',
   title: 'ip copied to clipboard',
   showConfirmButton: false,
   timer: 1500
 })
-console.log("Anti Ark website made by Austin287856");
-const element = document.getElementById('ip_button');
-element.addEventListener("click", () => {
-	navigator.clipboard.writeText("play.pretty-hot.xyz").then(function() {
  	alert('ip has been set to clipboard');
 }, function() {
   alert('samsung smart fridge user confirmed');
