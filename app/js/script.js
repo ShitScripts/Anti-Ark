a1c3697526ee58dad3c8227ebf889f6b7e8b1ae7
austin powers <35671313+ShitScripts@users.noreply.github.com>
2022-07-22 04:38:52 -0700

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,8 @@
-console.log("Website by Austin287856");
+console.log("Website by Austin287856 V1");
 
 var btn1 = document.querySelector('.btn1');
 
 btn1.addEventListener('click', function(event) {
-  try {
-    navigator.clipboard.writeText('play.pretty-hot.xyz');
-  } catch (err) {
-    console.log('Oops, unable to copy');
-  }
+  console.log('detected click')
+  navigator.clipboard.writeText('play.pretty-hot.xyz');
 });
